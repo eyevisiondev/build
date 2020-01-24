f40863b72f1fa15465d4d73fb3d3ac6b3c9b9340
victor <victorgvela@hotmail.com>
2020-01-24 10:06:28 -0300

    Initial commit

---
+++ precache-manifest.340d44e92ec7543ea60898bc6c3ca264.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebc9ab4f2b524b8ad44fbabddf71b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50191b99a0865ec00052",
+    "url": "/static/css/main.d83418f6.chunk.css"
+  },
+  {
+    "revision": "7a95ec4ac10979e9f718",
+    "url": "/static/js/2.8b65f589.chunk.js"
+  },
+  {
+    "revision": "50191b99a0865ec00052",
+    "url": "/static/js/main.22e0f505.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6d569cf695417080763fdc3f71bdc6b",
+    "url": "/static/media/250.d6d569cf.png"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "cdee81174a5647ef49e01929fca6d7fc",
+    "url": "/static/media/back_temp.cdee8117.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "557e3b5c2e61adf51e177352de270935",
+    "url": "/static/media/fernando.557e3b5c.jpg"
+  },
+  {
+    "revision": "cc9498472ceaaa4aa6c98c3a22c8355b",
+    "url": "/static/media/flavio.cc949847.jpg"
+  },
+  {
+    "revision": "20ab33d68b24c4caa64aa614734a5666",
+    "url": "/static/media/fundo_logo.20ab33d6.jpg"
+  },
+  {
+    "revision": "14753b9347a9190f26cb04fb77110ce8",
+    "url": "/static/media/giovani.14753b93.jpeg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/static/media/uriel-soberanes.fe3a7838.jpg"
+  },
+  {
+    "revision": "227706b58899cb4db6c65a5a5dadb195",
+    "url": "/static/media/victor.227706b5.png"
+  }
+]);
